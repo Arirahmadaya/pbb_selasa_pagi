2aef3793a4c973d9f619486dcb06cdeba6ce8de2
arirahmadaya <arirahmadaya123@gmail.com>
2022-11-08 11:10:11 +0700

    Pertemuan 6

---
+++ latihan_listview/lib/main.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home:  ListViewPage(),
+    );
+  }
+}
+
+class ListViewPage extends StatefulWidget {
+  const ListViewPage({super.key});
+
+  @override
+  State<ListViewPage> createState() => _ListViewPageState();
+}
+
+class _ListViewPageState extends State<ListViewPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+
+          for(int i=1; i<=10; i++)...[
+            Container(
+              height: 250,
+              decoration: BoxDecoration(
+                //THIS IF ELSE VERSI SINGKAT
+                color:i%2 !=0 ? Colors.yellow : Colors.blue
+              ),
+              child: Center(
+                child: Text('$i', style: TextStyle(
+                  fontSize: 40, 
+                  ),
+                ),
+              ),
+            )
+          ]
+        ],
+      ),
+      /*body: ListView(
+        scrollDirection: Axis.horizontal,
+        children: [
+          Container(
+            width: 500,
+            height: 250,
+            decoration: BoxDecoration(color: Colors.yellow[200],
+            ),
+            child: Center(
+              child: Text("1",
+              style: TextStyle(
+                fontSize:10, color: Colors.green),
+                ),
+            ),
+          ),
+          Container(
+            width: 500,
+            height: 250,
+            decoration: BoxDecoration(color: Colors.yellow[400],
+            ),
+            child: Center(
+              child: Text("2",
+              style: TextStyle(
+                fontSize:10, color: Colors.green),
+                ),
+            ),
+          ),
+          Container(
+            width: 500,
+            height: 250,
+            decoration: BoxDecoration(color: Colors.yellow[500],
+            ),
+            child: Center(
+              child: Text("3",
+              style: TextStyle(
+                fontSize:10, color: Colors.green),
+                ),
+            ),
+          ),
+          Container(
+            width: 500,
+            height: 250,
+            decoration: BoxDecoration(color: Colors.red,
+            ),
+            child: Center(
+              child: Text("4",
+              style: TextStyle(
+                fontSize:100, color: Colors.green),
+                ),
+            ),
+          ),
+        ],),*/
+
+    );
+  }
+}
